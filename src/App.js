0a64153a0f893af0647dacf43cec818720f4ffbf
Alex Kaminski <alexdkaminski@gmail.com>
2019-07-23 16:09:16 +1000

    Add syntax highlighting

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
 import React from 'react';
 import marked from 'marked';
+import hljs from 'highlight.js';
 import './App.css';
 
 class MarkdownPreview extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      markdown: this.getMarkdownText("")
+      markdown: 
+      `# React Markdown Previewer
+## Free Code Camp Front End Library Project
+[FreeCodeCamp.com](https://learn.freecodecamp.org/front-end-libraries/front-end-libraries-projects/build-a-markdown-previewer)\n
+Inline \`code\`
+\`\`\`
+function addTwoNumbers(a, b) {
+  return a + b
+}
+\`\`\`
+`
     };
     this.handleChange = this.handleChange.bind(this)
   }
   handleChange(e) {
     this.setState({
-      markdown: this.getMarkdownText(e.target.value)
+      markdown: e.target.value
     })
   }
-  getMarkdownText(markdown) {
-    var rawMarkup = marked(markdown,{sanitize: true});
+  getMarkup() {
+    marked.setOptions({
+      renderer: new marked.Renderer(),
+      highlight: function(code) {
+        return hljs.highlightAuto(code).value;
+      },
+      breaks: true
+    });
+    var rawMarkup = marked(this.state.markdown);
     return { __html: rawMarkup };
   }
   render() {
     return(
       <div className="markdown-preview-wrapper">
         <div className="editor-wrapper">
-          <textarea id="editor" onChange={this.handleChange}></textarea>
+          <textarea id="editor" onChange={this.handleChange} value={this.state.markdown}></textarea>
         </div>
         <div className="preview-wrapper">
-          <div id="preview" dangerouslySetInnerHTML={this.state.markdown}>
+          <div id="preview" dangerouslySetInnerHTML={this.getMarkup()}>
           </div>
         </div>
       </div>

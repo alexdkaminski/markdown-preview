84be8c6d1a83e8d38b404ccda36cacfadc3ebd55
Alex Kaminski <alexdkaminski@gmail.com>
2019-07-26 12:03:42 +1000

    Add toggle buttons to hide/show editor and preview

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import marked from 'marked';
 import hljs from 'highlight.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faExpand } from '@fortawesome/free-solid-svg-icons';
+import { faCompress } from '@fortawesome/free-solid-svg-icons';
 import './App.scss';
 
 class MarkdownPreview extends React.Component {
@@ -16,10 +19,12 @@ Inline \`code\`
 function addTwoNumbers(a, b) {
   return a + b
 }
-\`\`\`
-`
+\`\`\``,
+      expanded: ""
     };
     this.handleChange = this.handleChange.bind(this)
+    this.togglePreview = this.togglePreview.bind(this)
+    this.toggleEditor = this.toggleEditor.bind(this)
   }
   handleChange(e) {
     this.setState({
@@ -37,32 +42,190 @@ function addTwoNumbers(a, b) {
     var rawMarkup = marked(this.state.markdown);
     return { __html: rawMarkup };
   }
+  togglePreview() {
+    if (this.state.expanded === "editor" || this.state.expanded === "") {
+      this.setState ({
+        expanded: "preview"
+      })
+    } else {
+      this.setState ({
+        expanded: ""
+      })
+    } 
+  }
+  toggleEditor() {
+    if (this.state.expanded === "preview" || this.state.expanded === "") {
+      this.setState ({
+        expanded: "editor"
+      })
+    } else {
+      this.setState ({
+        expanded: ""
+      })
+    } 
+  }
   render() {
     return(
       <section className="section">
         <div className="container">
           <div className="columns is-variable is-8">
-            <div className="editor-wrapper column">
-              <div className="title">
-                <h3>Editor</h3>
-                <i>Icon</i>
-              </div>
-              <textarea id="editor" onChange={this.handleChange} value={this.state.markdown}></textarea>
-            </div>
-            <div className="preview-wrapper column">
-            <div className="title">
-              <h3>Preview</h3>
-              <i>Icon</i>
-            </div>
-              <div id="preview" dangerouslySetInnerHTML={this.getMarkup()}>
-              </div>
-            </div>
+            <Editor expanded={this.state.expanded} toggleEditor={this.toggleEditor} markdown={this.state.markdown} handleChange={this.handleChange}  />
+            <Preview expanded={this.state.expanded} togglePreview={this.togglePreview} getMarkup={this.getMarkup()} />
           </div>
         </div>
       </section>
     )
   }
 }
+
+const Editor = (props) => {
+
+    var classes = (props.expanded === "editor") ? "editor-wrapper column" : (props.expanded === "preview") ? "editor-wrapper hide" : "editor-wrapper column" 
+
+    return (
+      <div className={classes}>
+        <div className="title">
+          <h3>Editor</h3>
+          <EditorToggle expanded={props.expanded} toggleExpand={props.toggleEditor}/>
+        </div>
+        <textarea id="editor" onChange={props.handleChange} value={props.markdown}></textarea>
+      </div>
+    )
+}
+
+const Preview = (props) => {
+
+  var classes = (props.expanded === "preview") ? "preview-wrapper column" : (props.expanded === "editor") ? "preview-wrapper hide" : "preview-wrapper column" 
+
+  return (
+    <div className={classes}>
+      <div className="title">
+        <h3>Preview</h3>
+        <PreviewToggle expanded={props.expanded} toggleExpand={props.togglePreview}/>
+      </div>
+      <div id="preview" dangerouslySetInnerHTML={props.getMarkup}>
+      </div>
+    </div>
+  )
+}
+
+// Stateless functional component
+const EditorToggle = (props) => {
+  if (props.expanded === "editor") {
+    return (
+      <span className="icon" onClick={props.toggleExpand}>
+        <FontAwesomeIcon icon={faCompress} />
+      </span>
+    )
+  } else {
+    return (
+      <span className="icon" onClick={props.toggleExpand}>
+        <FontAwesomeIcon icon={faExpand} />
+      </span>
+    )
+  }
+}
+
+// Stateless functional component
+const PreviewToggle = (props) => {
+  if (props.expanded === "preview") {
+    return (
+      <span className="icon" onClick={props.toggleExpand}>
+        <FontAwesomeIcon icon={faCompress} />
+      </span>
+    )
+  } else {
+    return (
+      <span className="icon" onClick={props.toggleExpand}>
+        <FontAwesomeIcon icon={faExpand} />
+      </span>
+    )
+  }
+}
+
+// Using state to toggle an element
+class ToggleButton1 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+
+    this.toggleVisibility = this.toggleVisibility.bind(this)
+
+  }
+
+  toggleVisibility() {
+    if (this.state.visibility) {
+      this.setState({
+        visibility: false
+      })
+    } else {
+      this.setState({
+        visibility: true
+      })
+    }
+  }
+
+  render() {
+    if (this.state.visibility) {
+      return (
+        <span className="icon"  onClick={this.toggleVisibility}>
+          <FontAwesomeIcon icon={faCompress} />
+        </span>
+      );
+    } else {
+      return (
+        <span className="icon"  onClick={this.toggleVisibility}>
+          <FontAwesomeIcon icon={faExpand} />
+        </span>
+      );
+    }
+  }
+};
+
+// Using state to toggle an element
+class ToggleButton2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      visibility: false
+    };
+
+    this.toggleVisibility = this.toggleVisibility.bind(this)
+
+  }
+
+  toggleVisibility() {
+    if (this.state.visibility) {
+      this.setState({
+        visibility: false
+      })
+    } else {
+      this.setState({
+        visibility: true
+      })
+    }
+  }
+
+  render() {
+    if (this.state.visibility) {
+      return (
+        <span className="icon"  onClick={this.toggleVisibility}>
+          <FontAwesomeIcon icon={faCompress} />
+        </span>
+      );
+    } else {
+      return (
+        <span className="icon"  onClick={this.toggleVisibility}>
+          <FontAwesomeIcon icon={faExpand} />
+        </span>
+      );
+    }
+  }
+};
+
+
 
 function App() {
   return (
